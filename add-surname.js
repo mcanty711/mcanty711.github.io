f3500fafbb056fe969b2229e260050f64baf180c
Marcus Canty <42560051+mcanty711@users.noreply.github.com>
2018-10-04 18:07:06 -0400

    Update add-surname.js

--- add-surname.js
+++ add-surname.js
@@ -1,10 +1,10 @@
 /* Prompts user to add last name */
 function tipUsername() {                          // Declare function
-  elMsg.className = 'tip';                        // Change class for message
-  elMsg.innerHTML = 'Please include your last name'; // Add message
+  //elMsg.className = 'tip';                        // Change class for message
+  elMsg.innerHTML = 'Please remember to include your last name'; // Add message
 }
 
-var el = document.getElementById('name');     // Username input
+var el = document.getElementById('names');     // Username input
 var elMsg = document.getElementById('feedback');  // Element to hold message
 
 // When the username input gains / loses focus call functions above:
